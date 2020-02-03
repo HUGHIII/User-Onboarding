7284765a2d54153873eabf0c01a8d86b3c0addb9
TREY <hugh.a.lloyd@gmail.com>
2020-02-02 21:57:07 -0600

    mvp plus one stretch

--- formik-useronboarding/src/Form.js
+++ formik-useronboarding/src/Form.js
@@ -10,7 +10,13 @@ import axios from 'axios'
 
 
 
-const UserForm = () => {
+const UserForm = ({values,errors,touched,status}) => {
+ const [users, setUsers] = useState([]);
+ useEffect(()=>{
+     console.log('status has changed', status);
+     status && setUsers(users => 
+        [...users, status])
+ },[status])
 
     return (
         <div className='form-div'>
@@ -19,6 +25,7 @@ const UserForm = () => {
                 <Field type='text'
                 name='name'
                 />
+                {touched.name && errors.name && <p className='errors'>{errors.name}</p>}
                 </label>
 
 
@@ -26,6 +33,7 @@ const UserForm = () => {
                 Email
                 <Field type='email'
                 name='email' />
+                {touched.email && errors.email && <p className='errors'>{errors.email}</p>}
             </label>
 
 
@@ -33,23 +41,28 @@ const UserForm = () => {
                 Password
                 <Field type='password'
                 name='password' />
+                {touched.password && errors.password && <p className='errors'>{errors.password}</p>}
             </label>
 
             <label>
                 Role
                 <Field as='select' name='role'>
-                    <option disabled='chooseRoll'>choose your role in the forum</option>
-                    <option value='comedian'>comedian</option>
-                    <option value='superiority complex'>superiority complex</option>
-                    <option value='troll'>troll</option>
+                    <option value ='chooseRole'>choose your role</option>
+                    <option value='frontEndDev'>Front End Dev</option>
+                    <option value='backEndDev'>Back End Dev</option>
+                    <option value='dataScience'>Data Scientist</option>
+                    <option value='other'>other</option>
                 </Field>
             </label>
+            
+            {/* if 'choose role' option in dropdown box is disabled then then the first actual option(frontEndDev) will already be filled in though if nothing else is selected it will still post an empty string to the api */}
 
 
             <label>
                 Agree to Terms of Service
                 <Field type='checkbox'
                 name='terms' />
+                {touched.terms && errors.terms && <p className='errors'>{errors.terms}</p>}
             </label>
 
 
@@ -60,19 +73,18 @@ const UserForm = () => {
 
             </Form>
 
+            {users.map(user => (
+                <ul key={user.id}>
+                    <li>Name: {user.name}</li>
+                    <li>Email: {user.email}</li>
+                    <li>Role: {user.role}</li>
+                </ul>
+            ))}
+
         </div>
     )
 
 }
-
-
-
-
-
-
-
-
-
 
 const FormikForm = withFormik({
     mapPropsToValues({name, email, password, role, terms}){
@@ -87,7 +99,22 @@ const FormikForm = withFormik({
 
 },
 validationSchema:Yup.object().shape({
-    name:Yup.string().required()
-})
+    name: Yup.string().required('you must enter a name in order to sign up'),
+    email: Yup.string().email().required('you must enter an email in order to sign up'),
+    password: Yup.string().min(20).required('you must enter a password in order to sign up'),
+    terms:  Yup.boolean().oneOf([true], 'you must accept our terms of service in order to sign up'),
+    
+}),
+
+handleSubmit(values, {setStatus}) {
+    console.log('submitting', values);
+    axios.post('https://reqres.in/api/users_',values)
+    .then(res => {
+        console.log('post is working', res)
+        setStatus(res.data)
+    })
+    .catch(err => console.log(err.res))
+}
+
 })(UserForm)
 export default FormikForm
